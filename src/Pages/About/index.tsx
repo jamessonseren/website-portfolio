f0aaa2c515f8cd398a33a8a2f8d30cc3cb78f229
Jamesson Seren de Arruda <jamesson.arruda@sga.pucminas.br>
2023-04-11 20:02:04 -0400

    general updates

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -9,7 +9,7 @@ export default function About(){
                     I have a degree from the Merchant Marine Officers School and worked as a Nautical Officer on a gas tanker for four years. 
                     In this role, I worked with mixed crews from all over the world, developing my communication and teamwork skills. 
                     This experience also taught me to think quickly in high-stress situations and make important decisions efficiently.<br/><br/>
-                    Currently, I am studying Information Systems and work with various technologies such as ReactJS, NextJS, NodeJS, and relational and non-relational databases, among others.
+                    Currently, I am attending Computer Science Bachelor's Degree at PUC Minas University and work with various technologies such as ReactJS, NextJS, NodeJS, and relational and non-relational databases, among others.
                     I am always looking to improve and advance my knowledge in technology, which allows me to be efficient and effective in any project.
                     I am open to new opportunities! If you have any questions, please do not hesitate to contact me.</p>
                 <img src="/Assets/Images/author-about.jpg" alt="author image" />
